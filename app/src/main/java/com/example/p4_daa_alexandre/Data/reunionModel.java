0dcf6ec04244dfa0ed43ceb68b2c80ccd6478d9e
AlexQobuz <alexandre.clemencot@qobuz.com>
2023-04-05 15:04:26 +0200

    Création du model de réunion.

---
+++ app/src/main/java/com/example/p4_daa_alexandre/Data/reunionModel.java
@@ -0,0 +1,10 @@
+package com.example.p4_daa_alexandre.Data;
+
+public class reunionModel {
+
+    private Integer hour;
+    private String place;
+    private String about;
+    private String participant;
+
+}
