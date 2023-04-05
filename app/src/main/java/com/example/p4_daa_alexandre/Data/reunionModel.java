1079476aa4d28fce755f162f03fce581d31dc28e
AlexQobuz <alexandre.clemencot@qobuz.com>
2023-04-05 15:48:36 +0200

    modification du model de réunion.

--- app/src/main/java/com/example/p4_daa_alexandre/Data/reunionModel.java
+++ app/src/main/java/com/example/p4_daa_alexandre/Data/reunionModel.java
@@ -1,10 +1,45 @@
 package com.example.p4_daa_alexandre.Data;
 
+import java.util.Collections;
+import java.util.List;
+
 public class reunionModel {
 
-    private Integer hour;
-    private String place;
-    private String about;
-    private String participant;
+    public Integer getMhour() {
+        return mhour;
+    }
+
+    public void setMhour(Integer mhour) {
+        this.mhour = mhour;
+    }
+
+    public String getMplace() {
+        return mplace;
+    }
+
+    public void setMplace(String mplace) {
+        this.mplace = mplace;
+    }
+
+    public String getMabout() {
+        return mabout;
+    }
+
+    public void setMabout(String mabout) {
+        this.mabout = mabout;
+    }
+
+    public List<String> getMparticipant() {
+        return mparticipant;
+    }
+
+    public void setMparticipant(String mparticipant) {
+        this.mparticipant = Collections.singletonList(mparticipant);
+    }
+
+    private Integer mhour;
+    private String mplace;
+    private String mabout;
+    private List<String> mparticipant;
 
 }
